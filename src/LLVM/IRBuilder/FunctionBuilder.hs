8f8dde10236d006f5f82aa3e86d461a34711554c
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-05-11 17:31:07 +0300

    wip

--- src/LLVM/IRBuilder/FunctionBuilder.hs
+++ src/LLVM/IRBuilder/FunctionBuilder.hs
@@ -3,14 +3,10 @@
 
 module LLVM.IRBuilder.FunctionBuilder (
   FunctionBuilder (..),
-  appendInstr,
-  appendAnnotation,
   appendFunctionBuilderBlock,
 ) where
 
 import Common (Name)
-import LLVM.IRAnnotation (IRAnnotation)
-import LLVM.IRInstruction (IRInstruction)
 import LLVM.IRModule (IRAttribute, IRBlock, IRLinkage)
 import LLVM.IRType (IRType)
 
@@ -23,12 +19,6 @@ data FunctionBuilder = FunctionBuilder
   , functionBuilderAttributes :: [IRAttribute]
   }
   deriving (Show, Eq, Ord)
-
-appendInstr :: IRInstruction -> FunctionBuilder -> FunctionBuilder
-appendInstr = undefined
-
-appendAnnotation :: IRAnnotation -> FunctionBuilder -> FunctionBuilder
-appendAnnotation = undefined
 
 appendFunctionBuilderBlock :: IRBlock -> FunctionBuilder -> FunctionBuilder
 appendFunctionBuilderBlock block FunctionBuilder{..} =
