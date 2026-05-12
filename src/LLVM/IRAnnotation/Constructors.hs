b9c4e94cad5f772c6b6e3a54d251127d6c22ed59
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-05-12 19:58:45 +0300

    wip

--- src/LLVM/IRAnnotation/Constructors.hs
+++ src/LLVM/IRAnnotation/Constructors.hs
@@ -13,14 +13,18 @@ import LLVM.IRAnnotation (IRAnnotation (..))
 import LLVM.IRBuilder (IRBuilder, (<##>))
 
 {- | Create a single-line comment annotation.
+
 Usage: @emitAnnotation (comment "This is a comment")@
+
 Renders as: @; This is a comment@
 -}
 comment :: Text -> IRAnnotation
 comment = Comment
 
 {- | Create a multi-line comment block annotation.
+j
 Usage: @emitAnnotation (commentBlock ["Line 1", "Line 2", "Line 3"])@
+
 Renders as:
 > ; Line 1
 > ; Line 2
@@ -30,7 +34,9 @@ commentBlock :: [Text] -> IRAnnotation
 commentBlock = CommentBlock
 
 {- | Alternative syntax for inline comments using function composition.
+
 Usage: @reg <- withComment "comment explaining this" $ add i32 a b@
+
 Renders as: @%reg = add i32 %a, %b  ; comment explaining this@
 -}
 withComment :: Text -> IRBuilder a -> IRBuilder a
