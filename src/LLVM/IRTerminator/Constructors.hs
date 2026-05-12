c9f5e72ed6da9fbea75ad40867e293f1ff6b861b
Johannes Heikki Hilden <johannes@greatgraphics.se>
2026-05-12 19:34:53 +0300

    wip

--- src/LLVM/IRTerminator/Constructors.hs
+++ src/LLVM/IRTerminator/Constructors.hs
@@ -10,17 +10,22 @@ import Common (Name)
 import LLVM.IRBuilder (IRBuilder, setTerminator)
 import LLVM.IROperand (IRConstant, IROperand, IRTerminator (..))
 
+-- | Return from the current function with the given operand as the return value.
 ret :: IROperand -> IRBuilder ()
 ret op = setTerminator (IRet op)
 
+-- | Unconditionally branch to the named block.
 br :: Name -> IRBuilder ()
 br n = setTerminator (IBr n)
 
+-- | Conditionally branch to one of two blocks based on a boolean operand.
 condbr :: IROperand -> Name -> Name -> IRBuilder ()
 condbr op n1 n2 = setTerminator (ICondBr op n1 n2)
 
+-- | Branch to one of several blocks based on an integer operand, with a default target.
 switch :: IROperand -> Name -> [(IRConstant, Name)] -> IRBuilder ()
 switch op n cs = setTerminator (ISwitch op n cs)
 
+-- | Mark the current block as unreachable (undefined behaviour if executed).
 unreachable :: IRBuilder ()
 unreachable = setTerminator IUnreachable
